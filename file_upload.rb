84d897bcbf05642a778e112885f67be6e0790d54
zgfif <zihzag@gmail.com>
2019-03-04 22:51:50 +0200

    add file_upload

--- file_upload.rb
+++ file_upload.rb
@@ -5,5 +5,24 @@ require 'watir'
 class FileUpload
   include PageObject
 
-  page_url('http://the-internet.herokuapp.com/upload')  
+  page_url('http://the-internet.herokuapp.com/upload')
 end
+
+browser = Watir::Browser.new :firefox
+
+file_up = FileUpload.new browser, true
+
+file_name = 'lenna.png'
+
+File.write(file_name, '')
+
+path = File.expand_path(file_name)
+
+file_up.file_field(id: 'file-upload').set path
+
+
+file_up.button(id: 'file-submit').click
+
+sleep 5
+
+file_up.quit
