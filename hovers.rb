80a5051dedca20029dba778df28e9741fe56830d
zgfif <zihzag@gmail.com>
2019-03-05 22:22:00 +0200

    change hovers

--- hovers.rb
+++ hovers.rb
@@ -12,6 +12,12 @@ class Hover
   div(:second_img, class: 'figure', index: 1)
 
   div(:third_img, class: 'figure', index: 2)
+
+  link(:first_link, href: '/users/1')
+
+  link(:second_link, href: '/users/2')
+
+  link(:third_link, href: '/users/3')
 end
 
 browser = Watir::Browser.new :firefox
@@ -20,14 +26,20 @@ hover_page = Hover.new browser, true
 
 hover_page.first_img_element.hover
 
-sleep 2
+puts hover_page.first_link_element.present?
+
+sleep 1
 
 hover_page.second_img_element.hover
 
-sleep 2
+puts hover_page.second_link_element.present?
+
+sleep 1
 
 hover_page.third_img_element.hover
 
-sleep 2
+puts hover_page.third_link_element.present?
+
+sleep 1
 
 hover_page.quit
