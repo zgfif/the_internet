61cead6d8592d036d7ec4270da44b01781fddccd
zgfif <zihzag@gmail.com>
2019-03-07 10:16:09 +0200

    change file_downlaod

--- file_download.rb
+++ file_download.rb
@@ -2,10 +2,35 @@ require 'page-object'
 
 require 'watir'
 
+require 'fileutils'
+
 class FileDownload
   include PageObject
 
   page_url('http://the-internet.herokuapp.com/download')
+
+  def perform_downloads type
+    self.links.each do |link|
+      if link.href.match? type
+
+        link.click
+      end
+    end
+  end
+
+  def validate_downloaded_types type
+    Dir["temp/*"].each do |file|
+      if file.match type
+        puts true
+      else
+        puts false
+      end
+    end
+  end
+
+  def remove_files
+    FileUtils.rm_rf('temp/')
+  end
 end
 
 prefs = {
@@ -19,10 +44,12 @@ browser = Watir::Browser.new :chrome, options: { prefs: prefs }
 
 download = FileDownload.new browser, true
 
-download.links.each do |link|
-  if link.href.match? /.txt/
-    link.click
-  end
-end
+download.perform_downloads '.txt'
+
+sleep 5
+
+download.validate_downloaded_types '.txt'
+
+download.remove_files
 
 download.quit
